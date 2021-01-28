6abea809b308c5d24e5d9c435d35d9cddd9a9a79
chenweiyan <chanweiyan007@gmail.com>
2021-01-29 05:09:07 +0800

    rm bundler version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,11 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  #   Please check your Rails app for 'config.i18n.fallbacks = true'.
+  # remote:        If you're using I18n (>= 1.1.0) and Rails (< 5.2.2), this should be
+  # remote:        'config.i18n.fallbacks = [I18n.default_locale]'.
+  # heroku
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
